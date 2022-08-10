61ed863d95933fa83ea1307323783f9db24762b6
Artem K <tem-k@ya.ru>
2022-08-10 18:35:55 +0700

    first commit

---
+++ задача2_2/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите число");
+while (true)
+{
+    string input = Console.ReadLine();
+    if (!input.Equals("exit"))
+        Console.WriteLine("{0}->{1}", input, input[2]);
+    else
+        break;
+}
+
+// команда exit - выход
